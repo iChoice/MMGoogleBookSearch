cf82452208aaeaeea1133541a8f13405a635f093
MichaelMeinberg <michael.meinberg@me.com>
2020-03-27 09:31:43 -0700

    Updating the block to have weak self.

--- GoogleBookSearch/ViewModels/SearchForBookViewModel.swift
+++ GoogleBookSearch/ViewModels/SearchForBookViewModel.swift
@@ -16,38 +16,39 @@ class SearchForBookViewModel {
     func search (_ title:String, completionHandler: @escaping (NSError?) -> Void) {
    
         self.books = Books() // Clear out the books from the last search
-        self.getBooksApi.bookSearchFromGoogleBooks(title: title, completion: { (responseData,error)  in
-                if let v = try? JSONSerialization.jsonObject(with: responseData!, options: .allowFragments) as AnyObject {
-                    if let booksFoundAtGoogle = v["items"] as? [[String:AnyObject]] {
-                        for bookImported in booksFoundAtGoogle {
-                            let volumeInfo = bookImported["volumeInfo"]!
-                            print("volume Info: \(volumeInfo)")
-                            
-                            var imageLinks = ["":""]
-                            var smallThumbnailURL = ""
-                            if let imageURLs = volumeInfo["imageLinks"] as? [String:String] {
-                                imageLinks = imageURLs
-                                smallThumbnailURL = imageLinks["smallThumbnail"]!
-                            }
-                            
-                            let description = volumeInfo["description"] as? String ?? "" 
-                            
-                            let thumbnailImageURL =  imageLinks["thumbnail"]!
-                            var authors = [""] // Make sure their are authors listed
-                            if let a = volumeInfo["authors"] as? [String] {
-                                authors = a
-                            }
-                            let book = Book(title:volumeInfo["title"] as! String,
-                                            authors: authors ,
-                                            smallThumbnailImageURL:smallThumbnailURL,
-                                            thumbnailImageURL:thumbnailImageURL,
-                                            description:description)
-                            
-                            self.books.booksFound.append(book)
+        self.getBooksApi.bookSearchFromGoogleBooks(title: title, completion: {  [weak self]  (responseData,error)  in
+            guard let self = self else { return }
+            if let v = try? JSONSerialization.jsonObject(with: responseData!, options: .allowFragments) as AnyObject {
+                if let booksFoundAtGoogle = v["items"] as? [[String:AnyObject]] {
+                    for bookImported in booksFoundAtGoogle {
+                        let volumeInfo = bookImported["volumeInfo"]!
+                        print("volume Info: \(volumeInfo)")
+                        
+                        var imageLinks = ["":""]
+                        var smallThumbnailURL = ""
+                        if let imageURLs = volumeInfo["imageLinks"] as? [String:String] {
+                            imageLinks = imageURLs
+                            smallThumbnailURL = imageLinks["smallThumbnail"]!
                         }
+                        
+                        let description = volumeInfo["description"] as? String ?? ""
+                        
+                        let thumbnailImageURL =  imageLinks["thumbnail"]!
+                        var authors = [""] // Make sure their are authors listed
+                        if let a = volumeInfo["authors"] as? [String] {
+                            authors = a
+                        }
+                        let book = Book(title:volumeInfo["title"] as! String,
+                                        authors: authors ,
+                                        smallThumbnailImageURL:smallThumbnailURL,
+                                        thumbnailImageURL:thumbnailImageURL,
+                                        description:description)
+                        
+                        self.books.booksFound.append(book)
                     }
-                    completionHandler(nil)
                 }
+                completionHandler(nil)
+            }
         })
     }
 }
